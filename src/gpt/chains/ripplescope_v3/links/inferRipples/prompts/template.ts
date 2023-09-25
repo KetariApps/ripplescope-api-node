0354e453011ca780970edccc6ab37e5ef3d220bd
zachagarrett <zach.garrett.archery@gmail.com>
2023-09-25 11:45:39 -0500

    improve ripple naming

--- src/gpt/chains/ripplescope_v3/links/inferRipples/prompts/template.ts
+++ src/gpt/chains/ripplescope_v3/links/inferRipples/prompts/template.ts
@@ -4,7 +4,7 @@ import { GPT_RipplesResponse } from '../../../types.js';
 const responseObject: GPT_RipplesResponse = {
   ripples: [
     {
-      name: 'The name of the ripple.',
+      name: 'A name for the ripple in three words or less. Do not include sentiment in the name.',
       brief: 'A short summary of the ripple.',
       description:
         "A longer description of the ripple and how the organization's operations caused it on the Scope.",
