7a7c6340ede4f9d8bf6bd421c3dfaac09fc0ca83
zachagarrett <zach.garrett.archery@gmail.com>
2023-09-26 13:47:37 -0500

    exit on throw

--- src/startup/index.ts
+++ src/startup/index.ts
@@ -2,10 +2,6 @@ import { addConstraints } from './addConstraints.js';
 import { insertScopes } from './insertScopes.js';
 
 (async () => {
-  try {
-    await addConstraints();
-    await insertScopes();
-  } catch (error) {
-    console.error(error);
-  }
+  await addConstraints();
+  await insertScopes();
 })();
