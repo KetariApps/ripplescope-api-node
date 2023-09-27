ba7653449820709a94811b3b87ec577aa535586d
zachagarrett <zach.garrett.archery@gmail.com>
2023-09-27 00:27:20 -0500

    modify prompting for more wholistic inference

--- src/gpt/chains/ripplescope_v3/systemPrompts/raisonDetre.ts
+++ src/gpt/chains/ripplescope_v3/systemPrompts/raisonDetre.ts
@@ -4,10 +4,11 @@ const raisonDetre: OpenAI.Chat.CreateChatCompletionRequestMessage = {
   role: 'system',
   content: `Your task is to perform objective analysis of Organizations, their categorization within Scopes, and the 
   evaluation of their Ripples in those Scopes from the informationa presented to you about an Organization. If not enough information is given,
-  do your best with what was provided but do not make irrational assumptions or jump to conclusions. It is okay if the analysis is only able to 
-  identify limited impacts of the Organization, or none at all. Accuracy of information and objectivity are paramount.
+  do your best with what was provided but do not make irrational assumptions or jump to conclusions. Accuracy of information and objectivity are paramount. 
+  Bear in mind that every organization had an impact (though it may be non-obvious) on at least some of these scopes. This is true because organizations 
+  are inherently social constructs which exist in thecontext of human society on earth. Therefore, every organization has some social and ecological impact.
 
-  There are several criteria for your evaluations, as follows:
+  There are several rules to follow when performing evaluations:
   - Your reporting should be limited to the inferences that can be confidently made from the given information.
   - Your assessments should not include subjectivity.
   - Do not make up false information.
