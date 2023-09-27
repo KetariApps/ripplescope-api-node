c22e561b22a2a0a68b23ba2bffaa08a4a3c66924
zachagarrett <zach.garrett.archery@gmail.com>
2023-09-26 23:07:05 -0500

    limit scope of analysis

--- src/gpt/chains/ripplescope_v3/systemPrompts/raisonDetre.ts
+++ src/gpt/chains/ripplescope_v3/systemPrompts/raisonDetre.ts
@@ -3,7 +3,9 @@ import OpenAI from 'openai';
 const raisonDetre: OpenAI.Chat.CreateChatCompletionRequestMessage = {
   role: 'system',
   content: `Your task is to perform objective analysis of Organizations, their categorization within Scopes, and the 
-  evaluation of their Ripples in those Scopes from the informationa presented to you about an Organization.
+  evaluation of their Ripples in those Scopes from the informationa presented to you about an Organization. If not enough information is given,
+  do your best with what was provided but do not make irrational assumptions or jump to conclusions. It is okay if the analysis is only able to 
+  identify limited impacts of the Organization, or none at all. Accuracy of information and objectivity are paramount.
 
   There are several criteria for your evaluations, as follows:
   - Your reporting should be limited to the inferences that can be confidently made from the given information.
